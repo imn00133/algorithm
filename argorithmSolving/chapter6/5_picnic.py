94d1f1743562fedc401b016c4a2546b189f8cfd6
Kim Jaehyeong <imn00133@gmail.com>
2020-01-02 11:46:40 +0900

    solving picnic problem additionally rename to include subnumber

--- argorithmSolving/chapter6/5_picnic.py
+++ argorithmSolving/chapter6/5_picnic.py
@@ -12,19 +12,27 @@ def initiate(student, friend_num, friend_list):
 
 def count_pairing(are_friends, taken):
 	# are_friends는 global이 나을 수도...
+	first_student = -1
+	for i in range(len(taken)):
+		if not taken[i]:
+			first_student = i
+			break
 	# 기저 사례: 모든 학생이 짝을 찾으면 한 가지 방법을 찾아서 종료
-	if all(taken):
+	# all을 사용할 수 있으나, 앞쪽에서 검사를 했기 때문에 반복 검사하지 않는다.
+	if first_student == -1:
 		return 1
 	ret = 0
-	# 같은 학생 짝 두 번 짓는 것, 다른 순서로 학생들을 짝짓는 것을 해소해야함
-	# 두 번째 검사의 시작을 i 이후로 잡으면 다른 순서로 학생을 짝 짓는 것이 포함됨
-	for i in range(len(taken)):
-		for j in range(i, len(taken)):
-			if not taken[i] and not taken[j] and are_friends[i][j]:
-				taken[i] = taken[j] = True
-				# 재귀일 때, refernce 참조로 넘겨도 문제 없나 흐음...
-				ret += count_pairing(are_friends, taken)
-				taken[i] = taken[j] = False
+	# 전체를 순환하면서 돌면 같은 학생 짝 두 번 짓는 것, 다른 순서로 학생들을 짝짓는 것을 해소해야함
+	# 두 번째 검사의 시작을 i 이후만 잡으면 다른 순서로 학생을 짝 짓는 것이 포함됨
+	# 따라서 처음 사람을 지정하고 나머지 사람에 대해서 계산하도록 프로그램을 변경
+	for pair_with in range(first_student + 1, len(taken)):
+		if not taken[pair_with] and are_friends[first_student][pair_with]:
+			taken[first_student] = taken[pair_with] = True
+			# 재귀일 때, refernce 참조로 넘겨도 문제 없나 흐음...
+			# 사람처럼 한 번에 일련의 과정을 전체 탐색하는 것이 아님으로,
+			# 한 가지에 들어갔을 때 막고 풀면 문제 없음
+			ret += count_pairing(are_friends, taken)
+			taken[first_student] = taken[pair_with] = False
 	return ret
 
 
@@ -40,7 +48,7 @@ def main():
 
 def file_main():
 	# file로 검사하기 위한 main
-	file = open('test.txt', 'r', encoding='utf-8')
+	file = open('5_test.txt', 'r', encoding='utf-8')
 	case_num = int(file.readline())
 	for _ in range(case_num):
 		# 첫 번째는 학생 수, 두 번째는 친구 쌍의 수
@@ -52,4 +60,4 @@ def file_main():
 
 
 if __name__ == '__main__':
-	file_main()
+	main()
