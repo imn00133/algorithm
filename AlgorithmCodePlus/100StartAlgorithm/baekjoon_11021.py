016879ef1630a9992be495cbf2dc2006da2dcd68
Jaehyeong Kim <imn00133@gmail.com>
2020-03-19 21:17:39 +0900

    solving 11021 problem in baekjoon online judge

---
+++ AlgorithmCodePlus/100StartAlgorithm/baekjoon_11021.py
@@ -0,0 +1,7 @@
+# https://www.acmicpc.net/problem/11021
+
+test_case_num = int(input())
+for count in range(1, test_case_num+1):
+	a, b = map(int, input().split())
+	print("Case #{}: {}".format(count, a+b))
+# range 시작을 바꾸지 말고, count+1을 해도 된다.
