533533ab26c9b65f2126fa642c56beb21bf77791
Jaehyeong Kim <imn00133@gmail.com>
2020-03-19 21:44:11 +0900

    solving 11022 problem in baekjoon online judge

---
+++ AlgorithmCodePlus/100StartAlgorithm/baekjoon_11022.py
@@ -0,0 +1,8 @@
+# https://www.acmicpc.net/problem/11021
+
+test_case_num = int(input())
+for count in range(test_case_num):
+	# a, b = (int(x) for x in test_case_num.split())도 가능하다.
+	# 리스트 내포, 제너레이트 둘 다 가능.
+	a, b = map(int, input().split())
+	print("Case #{}: {} + {} = {}".format(count+1, a, b, a+b))
