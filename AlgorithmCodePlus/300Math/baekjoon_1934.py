a1434a0a7717371a2f471af64bd535cef61ff5d3
Jaehyeong Kim <imn00133@gmail.com>
2020-03-26 22:56:06 +0900

    solved 1934 problem in baekjoon online judge

---
+++ AlgorithmCodePlus/300Math/baekjoon_1934.py
@@ -0,0 +1,35 @@
+# https://www.acmicpc.net/problem/1934
+
+import sys
+read = sys.stdin.readline
+
+
+def gcm(a, b):
+	while b:
+		r = a % b
+		a = b
+		b = r
+	return a
+
+
+def lcm(a, b):
+	return int(a / gcm(a, b) * b)
+
+
+def main(mode=''):
+	if mode == 'f':
+		file = open("baekjoon_1934_input.txt", mode='r', encoding='utf-8')
+		test_case_num = int(file.readline().strip())
+		for _ in range(test_case_num):
+			a, b = (int(x) for x in file.readline().split())
+			print(lcm(a, b))
+		file.close()
+	else:
+		test_case_num = int(read().strip())
+		for _ in range(test_case_num):
+			a, b = (int(x) for x in read().split())
+			print(lcm(a, b))
+
+
+if __name__ == '__main__':
+	main()
