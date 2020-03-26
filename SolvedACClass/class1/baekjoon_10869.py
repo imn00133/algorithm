4640bdf2248b2abaef88466629bd55cf0022d40a
Jaehyeong Kim <imn00133@gmail.com>
2020-03-26 23:17:54 +0900

    solved 10171, 10172, 10869, 10998 problem in baekjoon online judge

---
+++ SolvedACClass/class1/baekjoon_10869.py
@@ -0,0 +1,9 @@
+import sys
+read = sys.stdin.readline
+
+a, b = (int(x) for x in read().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
---
+++ SolvedACClass/class1/baekjoon_10998.py
@@ -0,0 +1,6 @@
+import sys
+read = sys.stdin.readline
+
+# a, b = (int(x) for x in input().split())
+a, b = (int(x) for x in read().split())
+print(a * b)
