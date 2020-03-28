57cd6dce9bf79be16cbea2fd5c4feb6df18bb573
Jaehyeong Kim <imn00133@gmail.com>
2020-03-29 01:32:39 +0900

    solved 2743 problem in baekjoon online judge

---
+++ AlgorithmCodePlus/200DataStructure/Appendix/baekjoon_2743.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/2743
+# 오차범위내로 같다..?
+
+
+def legacy_len(string):
+	count = 0
+	for _ in range(len(string)):
+		count += 1
+	return count
+
+
+def pythonic_len(string):
+	return len(string)
+
+
+def main():
+	string = input()
+	print(pythonic_len(string))
+
+
+if __name__ == '__main__':
+	main()
