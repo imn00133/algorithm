34fae69f340d71997c35a08db8170a379613b938
Jaehyeong Kim <imn00133@gmail.com>
2020-03-29 03:10:33 +0900

    solved 11656 problem in baekjoon online judge

---
+++ AlgorithmCodePlus/200DataStructure/Appendix/baekjoon_11656.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/11656
+
+import sys
+read = sys.stdin.readline
+
+
+def main():
+	string = read().strip()
+	postfix_list = []
+	for index in range(len(string)):
+		postfix_list.append(string[index:])
+	postfix_list.sort()
+	for postfix in postfix_list:
+		print(postfix)
+
+
+if __name__ == '__main__':
+	main()
