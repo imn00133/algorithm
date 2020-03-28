ec264ba63a3501ec13318c067c1cd74f2909e744
Jaehyeong Kim <imn00133@gmail.com>
2020-03-28 22:30:09 +0900

    solved 2741, 2742 problem in baekjoon online judge

---
+++ SolvedACClass/class1/baekjoon_2741.py
@@ -0,0 +1,6 @@
+# https://www.acmicpc.net/problem/2741
+
+num = int(input())
+
+for index in range(1, num+1):
+	print(index)
---
+++ SolvedACClass/class1/baekjoon_2742.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/2742
+
+
+def used_reverse_range(num):
+	for index in range(num, 0, -1):
+		print(index)
+
+
+def not_used_range(num):
+	for index in range(1, num+1):
+		print(num+1 - index)
+
+
+def main():
+	num = int(input())
+	used_reverse_range(num)
+
+
+if __name__ == '__main__':
+	main()
