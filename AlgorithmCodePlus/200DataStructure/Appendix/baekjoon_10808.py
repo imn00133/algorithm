39cf2db6c3d4ad78f18826fa753d4492a0ded037
Jaehyeong Kim <imn00133@gmail.com>
2020-03-28 20:56:57 +0900

    solved 10808 problem in baekjoon online jugde

---
+++ AlgorithmCodePlus/200DataStructure/Appendix/baekjoon_10808.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/10808
+
+import sys
+read = sys.stdin.readline
+
+
+def main():
+	count_alphabet = [0 for _ in range(ord('z')-ord('a')+1)]
+	string = read().strip()
+	for letter in string:
+		count_alphabet[ord(letter)-ord('a')] += 1
+	for count in count_alphabet:
+		print(count, end=" ")
+
+
+if __name__ == '__main__':
+	main()
