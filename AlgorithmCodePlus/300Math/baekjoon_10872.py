994b559a4809749b51b48ca8963668aac3d997f2
Jaehyeong Kim <imn00133@gmail.com>
2020-03-28 20:09:08 +0900

    solved 10872 problem how to use recursive and loop in baekjoon online judge

---
+++ AlgorithmCodePlus/300Math/baekjoon_10872.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/10872
+
+
+def recursive_factorial(number):
+	if number == 0:
+		return 1
+	else:
+		return number * recursive_factorial(number-1)
+
+
+def loop_factorial(number):
+	factorial = 1
+	if number != 0:
+		for num in range(1, number+1):
+			factorial *= num
+	return factorial
+
+
+def main():
+	number = int(input())
+	print(recursive_factorial(number))
+
+
+if __name__ == "__main__":
+	main()
