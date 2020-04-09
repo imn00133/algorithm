f6b043930fed89f64f22d1d1797c5e26a37d02bd
Jaehyeong Kim <imn00133@gmail.com>
2020-04-09 23:08:56 +0900

    solved: 2156 problem to use bottom up dp in baekjoon online judge

--- AlgorithmCodePlus/400DynimicProgramming/Practice/baekjoon_2156.py
+++ AlgorithmCodePlus/400DynimicProgramming/Practice/baekjoon_2156.py
@@ -8,15 +8,22 @@ MAX = 10000
 dp_arr = [[0 for _ in range(3)] for _ in range(MAX+1)]
 
 
-def bottom_up(wines):
+def one_bottom_up(wines):
     dp_arr[0][0] = wines[0]
+    dp_arr[1][0] = wines[0] + wines[1]
+    for i in range(2, len(wines)):
+        dp_arr[i][0] = max(dp_arr[i-1][0], dp_arr[i-2][0] + wines[i], dp_arr[i-3][0] + wines[i] + wines[i-1])
+
+
+def bottom_up(wines):
+    dp_arr[0][1] = wines[0]
     for i in range(1, len(wines)):
         dp_arr[i][0] = max(dp_arr[i-1])
         dp_arr[i][1] = dp_arr[i-1][0] + wines[i]
-        dp_arr[i][2] = dp_arr[i-2][1] + wines[i]
+        dp_arr[i][2] = dp_arr[i-1][1] + wines[i]
 
 
-def top_down():
+def top_down(wines, index):
     pass
 
 
@@ -26,11 +33,13 @@ def main(mode=''):
     for _ in range(wine_num):
         wines.append(int(read().strip()))
     if mode == 'top':
-        top_down()
+        top_down(wines, len(wines)-1)
+    elif mode == 'one':
+        one_bottom_up(wines)
     else:
         bottom_up(wines)
     print(max(dp_arr[wine_num-1]))
 
 
 if __name__ == '__main__':
-    main()
+    main('one')
