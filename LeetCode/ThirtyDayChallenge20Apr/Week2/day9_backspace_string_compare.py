08e1ef7b8f62fd10c197ef27d5c470bcb74ed3cd
Jaehyeong Kim <imn00133@gmail.com>
2020-04-10 16:44:47 +0900

    sovled: day9 backspace string compare in leetcode

---
+++ LeetCode/ThirtyDayChallenge20Apr/Week2/day9_backspace_string_compare.py
@@ -0,0 +1,33 @@
+#
+# Solved Date: 20.04.10.
+
+
+def backspace_compare(S, T):
+    stack_s = []
+    stack_t = []
+    for letter in S:
+        if letter == '#':
+            if stack_s:
+                stack_s.pop()
+        else:
+            stack_s.append(letter)
+    for letter in T:
+        if letter == '#':
+            if stack_t:
+                stack_t.pop()
+        else:
+            stack_t.append(letter)
+    if stack_s == stack_t:
+        return True
+    else:
+        return False
+
+
+def main():
+    S = input()
+    T = input()
+    print(backspace_compare(S, T))
+
+
+if __name__ == '__main__':
+    main()
