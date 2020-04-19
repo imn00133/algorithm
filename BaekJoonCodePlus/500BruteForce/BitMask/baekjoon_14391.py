ca8873ab883a994b71e733665447a76c563852ed
Jaehyeong Kim <imn00133@gmail.com>
2020-04-19 22:13:26 +0900

    wrote: brute force test code in 14391 problem

--- BaekJoonCodePlus/500BruteForce/BitMask/baekjoon_14391.py
+++ BaekJoonCodePlus/500BruteForce/BitMask/baekjoon_14391.py
@@ -5,6 +5,34 @@ import sys
 read = sys.stdin.readline
 
 
+def baekjoon(n, m, a):
+    ans = 0
+    for s in range(1 << (n * m)):
+        sum = 0
+        for i in range(n):
+            cur = 0
+            for j in range(m):
+                k = i * m + j
+                if (s & (1 << k)) == 0:
+                    cur = cur * 10 + a[i][j]
+                else:
+                    sum += cur
+                    cur = 0
+            sum += cur
+        for j in range(m):
+            cur = 0
+            for i in range(n):
+                k = i * m + j
+                if (s & (1 << k)) != 0:
+                    cur = cur * 10 + a[i][j]
+                else:
+                    sum += cur
+                    cur = 0
+            sum += cur
+        ans = max(ans, sum)
+    return ans
+
+
 def bit_mask(row, col, paper):
     max_score = 0
     arr_size = row * col
@@ -42,5 +70,33 @@ def main():
     print(bit_mask(row, col, paper))
 
 
+def test():
+    for row in range(1, 4):
+        for col in range(1, 4):
+            print("row: {}, col: {} 실험 시작".format(row, col))
+            total_count = row * col
+            for value in range(10 ** total_count):
+                value_str = str(value)
+                temp = [0 for _ in range(row * col - len(value_str))]
+                temp.extend([int(x) for x in value_str])
+                paper = []
+                index = 0
+                next_index = col
+                for _ in range(row):
+                    paper.append(temp[index:next_index])
+                    index = next_index
+                    next_index += col
+                expect = baekjoon(row, col, paper)
+                real = bit_mask(row, col, paper)
+                if expect != real:
+                    print(paper)
+                    print("expect: {}".format(expect))
+                    print("real: {}".format(real))
+                if total_count >= 5 and value % 10000 == 0:
+                    print("{:0}퍼센트 진행".format(value / (10 ** total_count) * 100))
+            print("row: {}, col: {} 실험 종료".format(row, col))
+
+
 if __name__ == '__main__':
+    test()
     main()
