de1654112247e2f9d4cbc110f20f9a89a1547ae9
Jaehyeong Kim <imn00133@gmail.com>
2020-05-01 23:08:36 +0900

    solved: 2250 problem to use dfs through stack and recursion in baekjoon online judge

--- BaekJoonCodePlus/600Graph/BFS/baekjoon_2250.py
+++ BaekJoonCodePlus/600Graph/BFS/baekjoon_2250.py
@@ -3,6 +3,7 @@
 
 import sys
 from collections import defaultdict
+sys.setrecursionlimit(10 ** 4)
 
 read = sys.stdin.readline
 MAX = 10000
@@ -11,18 +12,20 @@ MAX = 10000
 def find_ans(tree_meta):
     min_level = MAX + 1
     max_width = 0
-    for level in tree_meta.keys():
+    meta_key = tree_meta.keys()
+    for level in sorted(meta_key):
+        # 맨 마지막이 최대, 맨 첫번째가 최소임이 보장된다.
         width = tree_meta[level][-1] - tree_meta[level][0] + 1
-        if max_width <= width:
-            min_level = min(min_level, level)
+        if max_width < width:
+            min_level = level
             max_width = width
     return min_level, max_width
 
 
 def explorer(adj_list, root):
-    # dfs
-    tree_mata = defaultdict(list)
-    # node, 왼쪽 순회 확인, 레벨
+    # defaultdict은 키가 없을 경우 빈 리스트를 작성하여 값을 초기화해준다.
+    tree_meta = defaultdict(list)
+    # node, 왼쪽 순회 점검, 레벨
     stack = [(root, False, 1)]
     column = 1
     while stack:
@@ -30,13 +33,24 @@ def explorer(adj_list, root):
         if node == -1:
             continue
         elif right_flag:
-            tree_mata[level].append(column)
+            # in order 순회를 함으로, column값을 저장한 뒤, 값을 1증가시킨다.
+            tree_meta[level].append(column)
             column += 1
             stack.append((adj_list[node][1], False, level + 1))
         else:
             stack.append((node, True, level))
             stack.append((adj_list[node][0], False, level + 1))
-    return tree_mata
+    return tree_meta
+
+
+def recursion_explorer(adj_list, tree_meta, node, column=1, level=1):
+    if node == -1:
+        return column
+    column = recursion_explorer(adj_list, tree_meta, adj_list[node][0], column, level+1)
+    tree_meta[level].append(column)
+    column += 1
+    column = recursion_explorer(adj_list, tree_meta, adj_list[node][1], column, level+1)
+    return column
 
 
 def check_root(adj_list):
@@ -53,18 +67,22 @@ def check_root(adj_list):
 
 def input_value():
     arr_num = int(read().strip())
-    adj_list = [[] for _ in range(arr_num + 1)]
+    adj_list = [None for _ in range(arr_num + 1)]
     for _ in range(arr_num):
-        temp = [int(x) for x in read().split()]
-        adj_list[temp[0]] = temp[1:]
+        node, *binary = (int(x) for x in read().split())
+        adj_list[node] = binary
     return adj_list
 
 
-def main():
+def main(mode=''):
     adj_list = input_value()
-    tree_meta = explorer(adj_list, check_root(adj_list))
+    if mode == 'recursive':
+        tree_meta = defaultdict(list)
+        recursion_explorer(adj_list, tree_meta, check_root(adj_list))
+    else:
+        tree_meta = explorer(adj_list, check_root(adj_list))
     print(*find_ans(tree_meta), sep=' ')
 
 
 if __name__ == '__main__':
-    main()
+    main('recursive')
