64a67bac5fa9d9ee8d6637cf731aa7bdb7112b2d
Kim Jaehyeong <imn00133@gmail.com>
2022-04-08 21:14:03 +0900

    partial_solve: moving block to use bfs in the ItIsCodingTest

--- ItIsCodingTest/chap13/22.moving_block.py
+++ ItIsCodingTest/chap13/22.moving_block.py
@@ -3,7 +3,36 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60063
 from collections import deque
 
-DXY = ((1, 0), (0, 1), (-1, 0), (0, -1))
+DXY_PARALLEL = ((1, 0), (0, 1), (-1, 0), (0, -1))
+
+
+def is_collision_wall(board, pos):
+    return board[pos[1]][pos[0]] == 1
+
+
+def is_collision_board(board, left, right):
+    return is_collision_wall(board, left) or is_collision_wall(board, right)
+
+
+def is_passed(time_board, pos, time):
+    return time_board[pos[1]][pos[0]] > time
+
+
+def is_passed_board(time_board, left, right, time):
+    return is_passed(time_board, left, time) and is_passed(time_board, right, time)
+
+
+def set_time_board_pos(time_board, pos, time):
+    time_board[pos[1]][pos[0]] = time
+
+
+def set_time_board(time_board, left, right, time):
+    set_time_board_pos(time_board, left, time)
+    set_time_board_pos(time_board, right, time)
+
+
+def new_position(pos, dxy):
+    return pos[0] + dxy[0], pos[1] + dxy[1]
 
 
 # 전부 1을 두르면 충돌만 판단
@@ -16,41 +45,47 @@ def new_board(board):
 
 
 def solution(board):
-    end_condition = (len(board[0]), len(board))
+    end_position = (len(board[0]), len(board))
     board = new_board(board)
     time_board = [[0 for _ in range(len(board[0]))] for _ in range(len(board))]
 
     queue = deque()
     queue.append(((1, 1), (2, 1), 0))
     while queue:
-        left, right, past_second = queue.popleft()
-        if left == end_condition or right == end_condition:
-            return past_second
+        left, right, second = queue.popleft()
+        current_second = second + 1
+        if left == end_position or right == end_position:
+            return second
 
-        second = past_second + 1
-        for dx, dy in DXY:
-            x, y = left
-            nx, ny = x + dx, y + dy
-            if board[ny][nx]:
+        for dxy in DXY_PARALLEL:
+            new_left, new_right = new_position(left, dxy), new_position(right, dxy)
+            if is_collision_board(board, new_left, new_right):
                 continue
 
-            if time_board[ny][nx] != 0 and second > time_board[ny][nx]:
+            if is_passed_board(time_board, new_left, new_right, current_second):
                 continue
 
-            time_board[ny][nx] = second
-            queue.append((left, (nx, ny), second))
+            set_time_board(time_board, new_left, new_right, current_second)
+            queue.append((new_left, new_right, current_second))
 
-        for dx, dy in DXY:
-            x, y = right
-            nx, ny = x + dx, y + dy
-            if board[ny][nx]:
+        # 빼서 바꾸면 상대 위치가 됨
+        relative_pos = left[1] - right[1], left[0] - right[0]
+        for _ in range(2):
+            relative_pos = (-relative_pos[0], -relative_pos[1])
+            new_left = new_position(left, relative_pos)
+            new_right = new_position(right, relative_pos)
+
+            # 평행이동했을 때 부딪히는지 확인
+            if is_collision_board(board, new_left, new_right):
                 continue
 
-            if time_board[ny][nx] != 0 and second > time_board[ny][nx]:
-                continue
+            if not is_passed_board(time_board, new_right, right, current_second):
+                set_time_board(time_board, new_right, right, current_second)
+                queue.append((new_right, right, current_second))
 
-            time_board[ny][nx] = second
-            queue.append(((nx, ny), right, second))
+            if not is_passed_board(time_board, left, new_left, current_second):
+                set_time_board(time_board, left, new_left, current_second)
+                queue.append((left, new_left, current_second))
 
 
 if __name__ == '__main__':
